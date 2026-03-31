15cace6b26aaf9e69afc39bf902d8cbec651ac25
Hai Nguyen <hn235930@gmail.com>
2026-03-31 14:51:41 +0700

    Time: 92 ms (78.77%) | Memory: 40.5 MB (63.69%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.kt
+++ 1894-merge-strings-alternately/merge-strings-alternately.kt
@@ -1,14 +1,17 @@
 class Solution {
     fun mergeAlternately(word1: String, word2: String): String {
         val result = StringBuilder()
-        val maxLength = maxOf(word1.length, word2.length)
-        for (i in 0 until maxLength) {
-            if (i < word1.length) {
-                result.append(word1[i])
-            }
-            if (i < word2.length) {
-                result.append(word2[i])
-            }
+        var i1 = 0
+        var i2 = 0
+        while (i1 < word1.length && i2 < word2.length) {
+            result.append(word1[i1++])
+                .append(word2[i2++])
+        }
+        if (word1.length < word2.length) {
+            result.append(word2.substring(i2))
+        }
+        if (word1.length > word2.length) {
+            result.append(word1.substring(i1))
         }
         return result.toString()
     }
