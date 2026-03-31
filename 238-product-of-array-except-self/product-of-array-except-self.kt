ffc57feff65ff3d9291fafbff014a950e1f0a6cf
Hai Nguyen <hn235930@gmail.com>
2026-03-31 17:38:51 +0700

    Time: 2 ms (99.48%) | Memory: 60.2 MB (81.04%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.kt
@@ -0,0 +1,32 @@
+class Solution {
+    fun productExceptSelf(nums: IntArray): IntArray {
+        var multi = 1
+        var countZero = 0
+        nums.forEach { num ->
+            if (num != 0) multi *= num
+            else {
+                countZero++
+            }
+        }
+        if (countZero > 0) {
+            if (countZero == nums.size) return nums
+            else {
+                if (countZero == 1) { 
+                    nums.forEachIndexed { index, i ->
+                        if (nums[index] == 0) nums[index] = multi
+                        else nums[index] = 0
+                    }
+                } else {
+                    nums.forEachIndexed { index, i ->
+                        nums[index] = 0
+                    }
+                }
+            }
+        } else {
+            nums.forEachIndexed { index, i ->
+                nums[index] = multi/nums[index]
+            }
+        }
+        return nums
+    }
+}
