0824b919ee8a82d4943661bbd5d02ec572305f5e
Hai Nguyen <hn235930@gmail.com>
2026-03-31 17:53:02 +0700

    Time: 2 ms (100.00%) | Memory: 133.3 MB (58.70%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun increasingTriplet(nums: IntArray): Boolean {
+        var first = Int.MAX_VALUE
+        var second = Int.MAX_VALUE
+        for (num in nums) {
+            when {
+                num <= first -> first = num
+                num <= second -> second = num
+                else -> return true
+            }
+        }
+        return false
+    }
+}
