426a3900a15f98f6357a9419ea087560e9344a51
Hai Nguyen <hn235930@gmail.com>
2026-03-31 16:40:26 +0700

    Time: 12 ms (20.08%) | Memory: 46.2 MB (9.62%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.kt
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.kt
@@ -1,17 +1,6 @@
 class Solution {
     fun kidsWithCandies(candies: IntArray, extraCandies: Int): List<Boolean> {
-        val result = mutableListOf<Boolean>()
-        var maxCandy = candies[0]
-        candies.forEach {candy ->
-            if (candy > maxCandy) {
-                maxCandy = candy
-            }
-        }
-        candies.forEach { candy ->
-            result.add(
-                candy + extraCandies >= maxCandy
-            )   
-        }
-        return result
+        val maxCandy = candies.maxOrNull() ?: 0
+        return candies.map { it + extraCandies >= maxCandy }
     }
 }
