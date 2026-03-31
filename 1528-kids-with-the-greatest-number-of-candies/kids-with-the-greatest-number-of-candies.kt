1c89de9c1a6c3e7de495d7dd8d37910a341557e9
Hai Nguyen <hn235930@gmail.com>
2026-03-31 16:35:17 +0700

    Time: 1 ms (100.00%) | Memory: 41.9 MB (92.05%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.kt
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.kt
@@ -1,13 +1,15 @@
 class Solution {
     fun kidsWithCandies(candies: IntArray, extraCandies: Int): List<Boolean> {
         var result = MutableList(candies.size) { false }
-        var maxx = candies.get(0)
-        for (i in 1 until candies.size) {
-            if (candies[i] > maxx)   maxx = candies.get(i)
+        var maxCandy = candies.get(0)
+        candies.forEach {candy ->
+            if (candy > maxCandy) {
+                maxCandy = candy
+            }
         }
-        for (i in 0 until candies.size) {
-            if (candies.get(i) + extraCandies >= maxx) {
-                result.set(i, true)
+        candies.forEachIndexed { index, candy ->
+            if (candy + extraCandies >= maxCandy) {
+                result.set(index, true)
             }
         }
         return result
