b2ef7b253daef11e4f9ebb0071ac65b1ca131ac5
Hai Nguyen <hn235930@gmail.com>
2026-03-31 17:27:09 +0700

    Time: 4 ms (99.40%) | Memory: 42.4 MB (96.01%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.kt
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.kt
@@ -1,5 +1,18 @@
 class Solution {
     fun reverseWords(s: String): String {
-        return s.trim().split(Regex("\\s+")).reversed().joinToString(" ")
+        val sb = StringBuilder()
+        var i = s.length - 1
+        while (i >= 0) {
+            while (i >= 0 && s[i] == ' ') i--
+            if (i < 0) break
+
+            val end = i
+
+            while (i >= 0 && s[i] != ' ') i--
+
+            if (sb.isNotEmpty()) sb.append(' ')
+            sb.append(s.substring(i+1, end+1))
+        }
+        return sb.toString()
     }
 }
