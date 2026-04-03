e0c105487582ca37702fdcf9c4840d674af5913d
Hai Nguyen <hn235930@gmail.com>
2026-04-03 10:07:18 +0700

    Time: 1 ms (97.21%) | Memory: 42.7 MB (6.74%) - LeetSync

--- 392-is-subsequence/is-subsequence.kt
+++ 392-is-subsequence/is-subsequence.kt
@@ -8,11 +8,7 @@ class Solution {
     fun haveSub(s: String, t: String, sIndex: Int, tIndex: Int): Boolean {
         if (tIndex == t.length || sIndex == s.length) return false
         if (s[sIndex] == t[tIndex]) {
-            if (sIndex == s.length - 1){
-                return true
-            } else {
-                return haveSub(s, t, sIndex+1, tIndex+1)
-            }
+            return if (sIndex == s.length-1) true else haveSub(s, t, sIndex+1, tIndex+1)
         } 
         return haveSub(s, t, sIndex, tIndex+1)
     }
