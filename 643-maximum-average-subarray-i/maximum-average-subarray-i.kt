d621d5e4dadd140e4749e9eab0effd67eb9a222f
Hai Nguyen <hn235930@gmail.com>
2026-04-10 19:29:33 +0700

    Time: 4 ms (87.74%) | Memory: 61.4 MB (66.51%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun findMaxAverage(nums: IntArray, k: Int): Double {
+        var sum = 0
+        for (i in 0 until k) {
+            sum += nums[i]
+        }
+
+        var maxSum = sum
+        for (i in k until nums.size) {
+            sum = sum - (nums[i-k] - nums[i])
+            maxSum = max(maxSum, sum)
+        }
+
+        return 1.0*maxSum / k
+    }
+}
